81087939df0c8fdc0233213ef50d9a3166df5872
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-09-24 16:13:48 +0900

    Navbar working

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@
  * 
 */
 
+const sections = document.querySelectorAll('section');
+const navBar = document.querySelector('#navbar__list');
+let activeSection = document.querySelector('.active-section');
+let activeNav = document.querySelector('.active-nav');
+const fragment = document.createDocumentFragment();
 
 /**
  * End Global Variables
@@ -25,6 +30,30 @@
  * 
 */
 
+function makeNavButton(section) {
+    const newNavButton = document.createElement('li');
+    newNavButton.classList.add('menu__link');
+    newNavButton.textContent = section.dataset.nav;
+    return newNavButton;
+}
+
+function setActiveNav(clickedNav) {
+    if (activeNav !== clickedNav) {
+        if (activeNav !== null) {
+            activeNav.classList.remove('active-nav');
+        }
+        clickedNav.classList.add('active-nav')
+        activeNav = clickedNav;
+    }
+}
+
+function setActiveSection(section) {
+    if (activeSection !== section) {
+        activeSection.classList.remove('active-section');
+        section.classList.add('active-section');
+        activeSection = section;
+    }
+}
 
 
 /**
@@ -48,7 +77,18 @@
  * 
 */
 
-// Build menu 
+// Build menu
+// TODO: set listener only on parent element, use target delegation
+for (const section of sections) {
+    const newNavButton = makeNavButton(section);
+    newNavButton.addEventListener('click', function() {
+        section.scrollIntoView({behavior: 'smooth'});
+        setActiveNav(newNavButton);
+        setActiveSection(section);
+    });
+    fragment.appendChild(newNavButton);
+}
+navBar.appendChild(fragment);
 
 // Scroll to section on link click
 
